10a54da14d9b67f52507aa57a8e7bf0421c51307
pb1983 <phil.bryer@gmail.com>
2023-08-03 20:18:44 -0400

    Added for loop to compare hour of the day to the dataset for each hour block. Added clock to top of page. Updated Raadme.md

--- Assets/script.js
+++ Assets/script.js
@@ -1,64 +1,79 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-//let calendarInfo = JSON.parse(localStorage.getItem("calendarInfo")) || [];
+let past = document.getElementsByClassName(".past");
+let present = document.getElementsByClassName(".present");
+let future = document.getElementsByClassName(".future");
+let currentHour = dayjs().hour();
+let hourTracker = document.querySelectorAll(".row");
+let saveButton = document.querySelectorAll(".saveBtn")
 
+
+
+//Keeps items in local storage to corresponding text boxes 
 
 document.querySelector('textarea[data-time="9"]').value = localStorage.getItem("9")
 document.querySelector('textarea[data-time="10"]').value = localStorage.getItem("10")
 document.querySelector('textarea[data-time="11"]').value = localStorage.getItem("11")
 document.querySelector('textarea[data-time="12"]').value = localStorage.getItem("12")
-document.querySelector('textarea[data-time="1"]').value = localStorage.getItem("1")
-document.querySelector('textarea[data-time="2"]').value = localStorage.getItem("2")
-document.querySelector('textarea[data-time="3"]').value = localStorage.getItem("3")
-document.querySelector('textarea[data-time="4"]').value = localStorage.getItem("4")
-document.querySelector('textarea[data-time="5"]').value = localStorage.getItem("5")
+document.querySelector('textarea[data-time="13"]').value = localStorage.getItem("13")
+document.querySelector('textarea[data-time="14"]').value = localStorage.getItem("14")
+document.querySelector('textarea[data-time="15"]').value = localStorage.getItem("15")
+document.querySelector('textarea[data-time="16"]').value = localStorage.getItem("16")
+document.querySelector('textarea[data-time="17"]').value = localStorage.getItem("17")
 
 
 
-let saveButton = $(".saveBtn");
+//Date and running clock
+
+function runningClock() {
+
+  let ticker = dayjs().format('[Today is ]dddd, MMMM D YYYY, hh:mm:ss a');
+
+  document.querySelector("#clock").textContent = ticker;
+
+}
+
+setInterval(runningClock, 1000);
 
 
+//color codes hours by past(grey), present(red), and future(green)
 
-saveButton.on("click", function(event) {
-  if (event.target.matches("button"))  {
-    
-    localStorage.setItem(this.dataset.time, this.previousElementSibling.value);
+function hourColors() {
 
-  } else if (event.target.matches("i")) {
-    localStorage.setItem(this.dataset.time, this.previousElementSibling.value);
-
-    // let userInput = this.previousElementSibling.value;
-    // let dataNumber = this.dataset.time;
+  for (let i = 0; i < hourTracker.length; i++) {
 
 
-    //calendarInfo.push(userInput, dataNumber);
-    //localStorage.setItem("calendarInfo", JSON.stringify(calendarInfo))
+    if (hourTracker[i].dataset.time == currentHour) {
+      hourTracker[i].classList.remove("past", "future")
+      hourTracker[i].classList.add("present")
+
+    } else if (hourTracker[i].dataset.time < currentHour) {
+      hourTracker[i].classList.remove("present", "future")
+      hourTracker[i].classList.add("past")
+
+    } else {
+      hourTracker[i].classList.remove("present", "past")
+      hourTracker[i].classList.add("future")
+    }
   }
-})
+}
+
+hourColors();
 
 
+//Puts text in local storage when a button or icon is clicked 
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+for (let i = 0; i < saveButton.length; i++) {
+  saveButton[i].addEventListener("click", function (event) {
 
+    if (event.target.matches("button")) {
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+      localStorage.setItem(this.dataset.time, this.previousElementSibling.value);
+
+    } else if (event.target.matches("i")) {
+
+      localStorage.setItem(this.dataset.time, this.previousElementSibling.value);
+
+    }
+  })
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Scheduler
+
+### This project is a day-planner application that allows the user to log the events of each hour of a typical 9-5 workday. The calendar is color-coded so that there is a clear visual representation of how much of the day has passed, the current hour, and how many hours are left. There is also a date and time stamp at the top of the page with continuous second movement. Methods used to complete this assignment include:
+
+* Use of local storage to keep user information in the text boxes after a page refresh.
+* Dayjs API for the onboard clock and time comparison for color-coding.
+* Class-selecting elements.
+* Navigating the DOM via element parents and siblings. 
+* Using for loops to iterate through objects that don't have unique identifiers. 
+
+
+
+[URL] - https://pb1983.github.io/Scheduler/
+
+
+Screenshot: 
